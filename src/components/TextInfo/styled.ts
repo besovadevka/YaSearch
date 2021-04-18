7513aa2a6e725f898afe61f2943bb55c58859323
Darya Tsimashenkava (@besovadevka) <timaph@mail.ru>
2021-04-18 15:10:56 +0300

    YS-06 fix: fix styles for text nfo component

--- src/components/TextInfo/styled.ts
+++ src/components/TextInfo/styled.ts
@@ -1,14 +1,11 @@
 import styled from 'styled-components';
 
 export const TextInfoWrapper = styled.div`
-  position: relative;
   width: 100%;
   height: 100%;
 
   p {
-    position: absolute;
-    top: 0;
-    left: 114px;
+    margin-left: 16px;
     font: normal 16px/20px 'Yandex Sans Display';
   }
 `;
