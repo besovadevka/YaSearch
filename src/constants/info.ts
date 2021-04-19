ca745c4c4fc67b4f4020de5a2c21cc341cdb4418
Darya Tsimashenkava (@besovadevka) <timaph@mail.ru>
2021-04-19 12:13:39 +0300

    master fix: fix fetch request

--- src/constants/info.ts
+++ src/constants/info.ts
@@ -39,16 +39,3 @@ export const MODAL_BOOK_ITEM_DATA = [
 export const API_URL = 'http://openlibrary.org/search.json?title=';
 export const API__COVER_URL = 'http://covers.openlibrary.org/b/id/';
 export const BOOKS_API_URL = 'https://openlibrary.org/';
-
-export const fetchOptions = {
-  method: 'GET',
-  mode: 'cors',
-  headers: {
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers':
-      'Access-Control-Allow-Headers, Origin, Accept, X-Requested-With, Content-Type, Authorization',
-    'Access-Control-Allow-Methods': 'GET, OPTIONS',
-    'Content-Type': 'application/json',
-  },
-  referrerPolicy: 'no-referrer',
-} as RequestInit;
