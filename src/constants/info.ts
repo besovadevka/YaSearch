3f4a6efe2b08d026ff908f6a4bbdabe46fb23a52
Darya Tsimashenkava (@besovadevka) <timaph@mail.ru>
2021-04-19 11:40:13 +0300

    MASTER hoxfix: headers...

--- src/constants/info.ts
+++ src/constants/info.ts
@@ -45,9 +45,11 @@ export const fetchOptions = {
   mode: 'cors',
   headers: {
     'Access-Control-Allow-Origin': 'https://yasearch-besovadevka.netlify.app/',
-    'Access-Control-Allow-Headers': '*',
-    'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
+    'Access-Control-Allow-Headers':
+      'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers',
+    'Access-Control-Allow-Methods': 'GET,HEAD,OPTIONS,POST,PUT',
     'Content-Type': 'application/json',
+    'Access-Control-Allow-Credentials': 'true',
   },
   referrerPolicy: 'no-referrer',
 } as RequestInit;
